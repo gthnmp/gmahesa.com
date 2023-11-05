336fe6358252526938b2b2495c4b8416295c977e
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-11-05 16:01:29 +0700

    Guestbook form redesign

--- components/GuestbookForm/index.tsx
+++ components/GuestbookForm/index.tsx
@@ -10,7 +10,7 @@ export default function GuestbookForm () {
   return (
     <form
       style={{ opacity: !pending ? 1 : 0.7 }}
-      className="relative w-full flex items-center"
+      className="relative w-full h-10 flex items-center"
       ref={formRef}
     >
       <input
@@ -20,14 +20,14 @@ export default function GuestbookForm () {
         name="entry"
         type="text"
         required
-        className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+        className="pl-2 py-2 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-bl-md rounded-tl-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
       />
       <button
-        className="flex items-center justify-center right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
+        className="flex items-center justify-center px-2 py-1 font-medium h-full bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded-tr-md rounded-br-md w-16"
         disabled={pending}
         type="submit"
       >
-        Sign
+        Send
       </button>
     </form>
   );
