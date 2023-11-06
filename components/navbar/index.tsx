f008950205868b31b3733c95638d8c00c222a659
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-11-06 12:37:23 +0700

    No sign-out button when there is no session

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,7 +3,7 @@ import { signOut } from "next-auth/react";
 import Link from "next/link";
 import {BiLogOut} from 'react-icons/bi'
 
-export default function Navbar () {
+export default function Navbar ({session} : {session : any}) {
   return(
     <nav className="absolute top-0 w-full h-20 flex justify-between items-center">
       <div className="flex gap-5 items-center">
@@ -11,10 +11,12 @@ export default function Navbar () {
         <Link href="/blog"> Blog </Link>
         <Link href="/guestbook"> Guestbook </Link>
       </div>
-      <button onClick={() => signOut()} className="bg-red-700 p-2 rounded-md flex items-center gap-2">
-        <BiLogOut/>
-        Sign Out
-      </button>
+      {session &&
+        <button onClick={() => signOut()} className="bg-red-700 p-2 rounded-md flex items-center gap-2">
+          <BiLogOut/>
+          Sign Out
+        </button>
+      }
     </nav>
   )
 }
