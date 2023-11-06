e53db1211812321ec81dbc1c9041e2e4b64c79f5
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-11-06 20:23:17 +0700

    responsive guestbook page

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -10,7 +10,7 @@ export default async function Guestbook() {
   const session = await getServerSession(authConfig) 
   const messages = await getMessages() as Message[]
   return (
-    <main className='w-full h-full flex flex-col gap-10'>
+    <main className='w-full h-full flex flex-col gap-10 px-4'>
       <div className='flex flex-col gap-5'>
         <h1 className='text-3xl font-medium'> sign my guestbook </h1>
         {session ? (
