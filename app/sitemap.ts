b963578a188aa0372e4d3b367ae4378d761d42f5
Gathan Mahesa <gathan.mahesa@gmail.com>
2023-11-21 21:21:01 +0700

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,15 @@
-export default async function sitemap() {
-  const routes = ['', '/blog', '/guestbook'].map(
-    (route) => ({
-      url: `https://gmahesa.com${route}`,
-      lastModified: new Date().toISOString().split('T')[0],
-    })
-  );
+import { getBlogPosts } from "@/utils/blog";
 
-  return [...routes];
+export default async function sitemap() {
+  let blogs = getBlogPosts().map((post) => ({
+    url: `https://gmahesa.com/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
+
+  let routes = ['', '/blog', '/guestbook'].map((route) => ({
+    url: `https://gmahesa.com${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  return [...routes, ...blogs];
 }
